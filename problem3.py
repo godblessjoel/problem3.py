66d38f5b5f8127193fa30f019feb9c7a6e9e3af6
godblessjoel <44238619+godblessjoel@users.noreply.github.com>
2018-12-18 10:11:09 -0500

    First commit

---
+++ problem3.py
@@ -0,0 +1,8 @@
+print('-'*60)
+print('Alien Greater Bot')
+print()
+answer = input('What planet are you from? ')
+if answer = 'Earth' :
+	print('Damn you ugly I thought you were an alien! ')
+else:
+	print('Welcome to Earth! Please do not kill us! ')
